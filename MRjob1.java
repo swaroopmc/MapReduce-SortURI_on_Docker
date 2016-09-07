30afca8ed2b87a458500ba03e61b8b895ff67c63
Swaroop Aradhya <mcswaroop.19@gmail.com>
2016-09-07 11:45:09 -0700

    Update MRjob1.java

--- MRjob1.java
+++ MRjob1.java
@@ -8,7 +8,7 @@ import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.*;
 import org.apache.hadoop.mapreduce.lib.output.*;
 
-public class Swaroop121 {
+public class MRjob1 {
     
     public static class MapperClass extends Mapper<Object, Text, Text, IntWritable>{
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
@@ -36,7 +36,7 @@ public class Swaroop121 {
     public static void main(String[] args) throws Exception{
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "URI count");
-        job.setJarByClass(Swaroop121.class);
+        job.setJarByClass(MRjob1.class);
         job.setMapperClass(MapperClass.class);
         job.setReducerClass(ReducerClass.class);
         job.setCombinerClass(ReducerClass.class);
