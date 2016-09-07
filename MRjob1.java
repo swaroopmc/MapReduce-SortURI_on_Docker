87f084e2db081718d86b7740453c4de688ee8666
swarooparadhyamc <mcswaroop.19@gmail.com>
2016-09-07 11:40:47 -0700

    first commit

---
+++ MRjob1.java
@@ -0,0 +1,52 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.apache.hadoop.mapreduce.lib.output.*;
+
+public class Swaroop121 {
+    
+    public static class MapperClass extends Mapper<Object, Text, Text, IntWritable>{
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
+            String s;
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while(itr.hasMoreTokens()){
+                s = itr.nextToken();
+                if(s.startsWith("/") && !(s.endsWith("/"))
+                    context.write(new Text(s), new IntWritable(1));
+            }
+        }
+    }
+    
+    public static class ReducerClass extends Reducer<Text, IntWritable, Text, IntWritable>{
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException{
+            int sum = 0;
+            for(IntWritable val : values){
+                sum += val.get();
+            }
+            context.write(key, new IntWritable(sum));
+        }
+    }
+    
+    
+    public static void main(String[] args) throws Exception{
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "URI count");
+        job.setJarByClass(Swaroop121.class);
+        job.setMapperClass(MapperClass.class);
+        job.setReducerClass(ReducerClass.class);
+        job.setCombinerClass(ReducerClass.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        //job.setOutputFormatClass(SequenceFileOutputFormat.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true)? 0:1);
+
+    }
+
+}
---
+++ MRjob2.java
@@ -0,0 +1,55 @@
+ 
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+
+public class Swaroop121Sort {
+
+
+ public static class MapSort extends Mapper<Text, Text, IntWritable, Text> {
+
+   		public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
+			context.write(new IntWritable(Integer.parseInt(value.toString())), key);			
+		}
+
+	}
+
+	public static class ReducerSort extends Reducer<IntWritable, Text, Text, IntWritable> {
+
+		public void reduce(IntWritable uricount,Iterable<Text> uri, Context context) throws IOException, InterruptedException {
+				for(Text eachuri: uri)
+					context.write(eachuri, uricount);
+			
+		}
+
+   }
+	
+	public static void main(String[] args) throws Exception {
+
+	Configuration conf1 = new Configuration();
+    Job job1 = Job.getInstance(conf1, "uri sort");
+    job1.setJarByClass(Swaroop121Sort.class);
+    job1.setMapperClass(MapSort.class);
+    job1.setReducerClass(ReducerSort.class);
+    job1.setMapOutputKeyClass(IntWritable.class);
+    job1.setMapOutputValueClass(Text.class);
+    job1.setInputFormatClass(KeyValueTextInputFormat.class);
+    job1.setOutputKeyClass(Text.class);
+    job1.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job1, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job1, new Path(args[1]));
+    System.exit(job1.waitForCompletion(true)? 0:1);
+    }
+  }
+
+ 
